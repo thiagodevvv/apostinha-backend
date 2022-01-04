6d727393ce48690a34ff731e166a0353033f56f1
thiago henrique da silva <th.ferrari@outlook.com.br>
2022-01-04 07:39:56 -0300

    first commit

--- index.js
+++
@@ -1,9 +0,0 @@
-// const db = require('./database/db')
-
-// (async () => {
-//     const {createTables} = require('./database/db')
-//     await createTables()
-// })()
-
-
-console.log("$2b$10$nOUIs5kJ7naTuTFkBy1veuK0kSxUFXfuaOKdOKf9xYT0KKIGSJwFa".length)
