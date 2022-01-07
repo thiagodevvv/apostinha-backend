da155a0dce4e8bdeaf51348b5446e94b0845a365
thiago henrique da silva <th.ferrari@outlook.com.br>
2022-01-06 22:38:27 -0300

    add bcrypt for password user

--- src/api/users.js
+++ src/api/users.js
@@ -1,10 +1,24 @@
-class Users {
-    signup(req, res) {
-        res.send('hello worldds')
-    }
+const schema = require('./functions/validateParamsSignUp')
+const connectDB = require('../../database/db')
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
+    
+async function SignUp(req, res) {
+
+    const response = schema.validate(req.body)
+    if(response.error)
+        return res.send(response.error)
+    bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
+            console.log(hash)
+    }).catch(err => {
+        console.log(err)
+    });
 }
 
 
 
-module.exports = new Users()
+
+module.exports = {
+    SignUp
+}
