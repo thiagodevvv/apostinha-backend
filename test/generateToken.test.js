a226e4938903a8fd922988a64eb1c9e722663631
thiago henrique da silva <th.ferrari@outlook.com.br>
2022-05-23 08:36:32 -0300

    fix await is not necessary and add test success generate token

--- test/generateToken.test.js
+++ test/generateToken.test.js
@@ -6,14 +6,21 @@ const generatePayload = require("../src/functions/generatePayload");
 const secret = process.env.SECRET;
 
 describe("#Generator token", () => {
+  it("#Sucess generate token", () => {
+    const payload = generatePayload(1);
+    const secret = process.env.SECRET;
+    const expected = generateToken(payload, secret);
+    const actual = 115;
+    assert.equal(actual, expected.length);
+  });
   it("Without param payload", async () => {
-    const token = await generateToken(null, secret);
+    const token = generateToken(null, secret);
     const expected = `Error: Expected "payload" to be a plain object.`;
     assert.equal(token, expected);
   });
   it("Without param secret", async () => {
     const payload = generatePayload(1);
-    const token = await generateToken(payload, null);
+    const token = generateToken(payload, null);
     const expected = "Error: secretOrPrivateKey must have a value";
     assert.equal(token, expected);
   });
